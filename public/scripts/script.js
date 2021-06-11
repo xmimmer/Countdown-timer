725de3d6c88b61ea294b75a08aaaecaced48ba7b
xmimmer <madsmoellerh4nsen@hotmail.com>
2021-06-11 16:18:39 +0200

    hummes bday

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,20 +3,20 @@ const hours = document.getElementById("hours")
 const minutes = document.getElementById("minutes")
 const seconds = document.getElementById("seconds")
 
-const MimmerBirthday = "16 October 2021"
+const MimmerBirthday = "8 February 2022"
 
 function countdown() {
-
-    const MimmerBirthdayDate = new Date(MimmerBirthday)
+    
     const currentDate = new Date()
+    const MimmerBirthdayDate = new Date(MimmerBirthday)
 
     const totalSeconds = (MimmerBirthdayDate - currentDate) / 1000;
-
+    
     const _days = Math.floor(totalSeconds / 3600 / 24);
     const _hours = Math.floor(totalSeconds / 3600) % 24;
     const _minutes = Math.floor(totalSeconds / 60) % 60;
     const _seconds = Math.floor(totalSeconds) % 60;
-
+    
     days.innerHTML = _days
     hours.innerHTML = _hours
     minutes.innerHTML = _minutes
